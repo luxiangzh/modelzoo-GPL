57d8a4b3b712fb464bfdb35fe789c314e2395bc1
Ryan <wangzhiwei73@huawei.com>
2022-03-21 16:03:48 +0800

    更新test文件

---
+++ built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch/test/train_yolov5x_performance_1p.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cur_path=`pwd`
+model_name=yolov5x
+batch_size=32
+
+for para in $*
+do
+   if [[ $para == --model_name* ]];then
+      	model_name=`echo ${para#*=}`
+   elif [[ $para == --batch_size* ]];then
+      	batch_size=`echo ${para#*=}`
+   fi
+done
+
+model_path="${cur_path}/models/${model_name}.yaml"
+
+source ${cur_path}/test/env_npu.sh
+
+taskset -c 0-23 python3.7 train.py \
+		--data data/coco.yaml \
+		--cfg $model_path \
+		--weights '' \
+		--batch-size $batch_size \
+		--device npu \
+		--npu 0 \
+		--epochs 2 > $cur_path/test/train_performance_1p.log 2>&1 &
