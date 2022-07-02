7fef692bb268b20c569494bf9e64fddb8352d6df
bailang <bailang12@h-partners.com>
2022-07-02 17:19:27 +0800

    [自研][PyTorch]Yolov5_for_PyTorch模型修改性能值过滤方式
    Signed-off-by: bailang <bailang12@h-partners.com>

--- built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch/test/train_yolov5s_full_1p.sh
+++ built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch/test/train_yolov5s_full_1p.sh
@@ -88,7 +88,7 @@ wait
 #结果打印，不需要修改
 echo "------------------ Final result ------------------"
 #输出性能FPS，需要模型审视修改
-FPS=`grep -a 'FPS'  $test_path_dir/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|awk '{print $3}'|awk -F "]" '{print $3}'|awk -F ":" '{print $2}'|tail -1`
+FPS=`grep -a 'FPS'  $test_path_dir/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|awk -F "FPS:" '{print $2}'|awk -F "]" '{print $1}'|tail -1`
 #打印，不需要修改
 echo "Final Performance images/sec : $FPS"
 
