a294a2b83964b9a89ea39d0c61e2a468ec139c57
Ryan <wangzhiwei73@huawei.com>
2022-07-06 10:43:27 +0800

    Yolov5_v3.1更新CIOU融合算子接口
    Yolov5_v3.1更新CIOU融合算子接口

--- built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch_v3.1/utils/general.py
+++ built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch_v3.1/utils/general.py
@@ -1,3 +1,36 @@
+# -*- coding: utf-8 -*-
+# BSD 3-Clause License
+#
+# Copyright (c) 2017
+# All rights reserved.
+# Copyright 2022 Huawei Technologies Co., Ltd
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# ==========================================================================
+
 import glob
 import logging
 import os
@@ -532,8 +565,8 @@ def compute_loss(p, targets, model):  # predictions, targets, model
             pwh = (pwh.sigmoid() * 2) ** 2 * (anchors[i].T)
             pbox = torch.cat((pxy, pwh), 0)  # predicted box
             
-            giou = bbox_iou(pbox, tbox[i], x1y1x2y2=False, CIoU=True) #iou
-            # giou = torch.npu_ciou(pbox, tbox[i], trans=True, is_cross=False).squeeze()
+            # giou = bbox_iou(pbox, tbox[i], x1y1x2y2=False, CIoU=True) #iou
+            giou = torch.npu_ciou(pbox, tbox[i], trans=True, is_cross=False).squeeze()
             giou = giou * (allmask) + (1. - allmask)
             lbox += (1.0 - giou).sum() / (sum_mask) # giou loss
             # Obj
