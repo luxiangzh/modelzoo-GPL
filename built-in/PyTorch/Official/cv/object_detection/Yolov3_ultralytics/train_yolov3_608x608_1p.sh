3183cb1b1b1977b4ec5c53e36b3f8c44fd575346
bailang <bailang12@h-partners.com>
2022-07-27 14:16:32 +0800

    [自研][PyTorch]Yolov3_ultralytics脚本整改
    Signed-off-by: bailang <bailang12@h-partners.com>

--- built-in/PyTorch/Official/cv/object_detection/Yolov3_ultralytics/train_yolov3_608x608_1p.sh
+++
@@ -1,2 +0,0 @@
-source env_npu.sh
-python3 train.py --data voc.yaml --cfg yolov3.yaml --weights '' --batch-size 32 --noval --img-size 608
--- built-in/PyTorch/Official/cv/object_detection/Yolov3_ultralytics/train_yolov3_tiny_640x640_8p.sh
+++
@@ -1,2 +0,0 @@
-source env_npu.sh
-python3 -m torch.distributed.launch --nproc_per_node 8 train.py --data voc.yaml --cfg yolov3-tiny.yaml --weights '' --batch-size 512 --noval
