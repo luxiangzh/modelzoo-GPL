7a58786a6d51f4564c1d08bd783717654acc1f84
weipengfei <weipengfei19@huawei.com>
2022-08-24 15:28:47 +0800

    添加license Signed-off-by: weipengfei <weipengfei19@huawei.com>

--- built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch_v3.1/utils/datasets.py
+++ built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch_v3.1/utils/datasets.py
@@ -1,3 +1,14 @@
+# Copyright (c) 2021
+# All rights reserved.
+# Copyright 2022 Huawei Technologies Co., Ltd
+#
+# This source code is licensed under the license found in the LICENSE file in
+# the root directory of this source tree. An additional grant of patent rights
+# can be found in the PATENTS file in the same directory.
+"""
+Dataloaders and dataset utils
+"""
+
 import glob
 import os
 import random
