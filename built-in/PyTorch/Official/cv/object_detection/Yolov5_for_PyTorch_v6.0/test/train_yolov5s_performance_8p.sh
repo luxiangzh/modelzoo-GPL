e60defcfe71a432c479f70da6f87f1266be2c1e5
dongwenbo6 <dongwenbo6@huawei.com>
2022-10-06 16:07:36 +0800

    test脚本整改
    test整改
    
    test修改

--- built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch_v6.0/test/train_yolov5s_performance_8p.sh
+++ built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch_v6.0/test/train_yolov5s_performance_8p.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+#网络名称,同目录名称,需要模型审视修改
+Network="yolov5s_v4.0"
+
 cur_path=`pwd`
 model_name=yolov5s
 batch_size=512
 
+for para in $*
+do
+   if [[ $para == --model_name* ]];then
+      	model_name=`echo ${para#*=}`
+   elif [[ $para == --batch_size* ]];then
+      	batch_size=`echo ${para#*=}`
+   fi
+done
+
 source ${cur_path}/test/env_npu.sh
+
+#训练开始时间，不需要修改
+start_time=$(date +%s)
+echo "start_time: ${start_time}"
 
 export MASTER_ADDR=127.0.0.1
 export MASTER_PORT=29500
@@ -42,3 +58,37 @@ do
 		                --local_rank $i > $cur_path/test/output/${i}/train_8p_${i}.log 2>&1 &
     fi                        
 done
+
+wait
+
+# #训练结束时间，不需要修改
+end_time=$(date +%s)
+echo "end_time: ${end_time}"
+e2e_time=$(( $end_time - $start_time ))
+
+#最后一个迭代FPS值
+FPS=`grep -a 'FPS:'  ${cur_path}/test/output/0/train_8p_0.log|awk 'END {print}'| awk -F "[" '{print $5}'| awk -F "]" '{print $1}'| awk -F ":" '{print $2}'`
+
+#打印，不需要修改
+echo "ActualFPS : $FPS"
+echo "E2E Training Duration sec : $e2e_time"
+
+#稳定性精度看护结果汇总
+#训练用例信息，不需要修改
+BatchSize=${batch_size}
+DeviceType=`uname -m`
+CaseName=${Network}_bs${BatchSize}_${RANK_SIZE}
+
+##获取性能数据，不需要修改
+#单迭代训练时长
+TrainingTime=`awk 'BEGIN{printf "%.2f\n", '${batch_size}'*1000/'${FPS}'}'`
+
+#关键信息打印到${CaseName}.log中，不需要修改
+echo "Network = ${Network}" > $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "RankSize = ${RANK_SIZE}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "BatchSize = ${BatchSize}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "DeviceType = ${DeviceType}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "CaseName = ${CaseName}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "ActualFPS = ${FPS}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "TrainingTime = ${TrainingTime}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "E2ETrainingTime = ${e2e_time}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
