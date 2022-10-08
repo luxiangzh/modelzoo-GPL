6e4606d4f976ffbcd8fb8aa8e7cdfffc5cf88675
dongwenbo6 <dongwenbo6@huawei.com>
2022-10-08 10:20:12 +0000

    !68 [自研][PyTorch][ Yolov5_for_PyTorch_v6.0]-test脚本整改 * test脚本整改

--- built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch_v6.0/test/train_yolov5s_eval.sh
+++ built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch_v6.0/test/train_yolov5s_eval.sh
@@ -5,7 +5,7 @@ Network="yolov5s_v4.0"
 
 cur_path=`pwd`
 model_name=yolov5s
-batch_size=128
+batch_size=256
 
 for para in $*
 do
@@ -16,29 +16,25 @@ do
    fi
 done
 
-#创建DeviceID输出目录，不需要修改
+# 校验是否指定了device_id,分动态分配device_id与手动指定device_id,此处不需要修改
 ASCEND_DEVICE_ID=0
 echo "device id is ${ASCEND_DEVICE_ID}"
-if [ -d ${cur_path}/test/output/${ASCEND_DEVICE_ID} ];
-	then
-	   rm -rf ${cur_path}/test/output/${ASCEND_DEVICE_ID}
-		mkdir -p ${cur_path}/test/output/${ASCEND_DEVICE_IDi}
-	else
-	   mkdir -p ${cur_path}/test/output/${ASCEND_DEVICE_ID}
-	fi
+
+#创建DeviceID输出目录，不需要修改
+if [ -d ${cur_path}/test/output/${ASCEND_DEVICE_ID} ];then
+    rm -rf ${cur_path}/test/output/${ASCEND_DEVICE_ID}
+    mkdir -p ${cur_path}/test/output/$ASCEND_DEVICE_ID/
+else
+    mkdir -p ${cur_path}/test/output/$ASCEND_DEVICE_ID/
+fi
+
+source ${cur_path}/test/env_npu.sh
 
 #训练开始时间，不需要修改
 start_time=$(date +%s)
 echo "start_time: ${start_time}"
 
-source ${cur_path}/test/env_npu.sh
-
-python3.7 -u train.py --data ./data/coco.yaml \
-                      --cfg yolov5s.yaml \
-                     --weights '' \
-                     --batch-size $batch_size \
-                     --device 0 \
-                     --epochs 2 > $cur_path/test/output/${ASCEND_DEVICE_ID}/train_perf_1p.log 2>&1 &
+python3 val.py --data ./data/coco.yaml --img-size 640 --weight 'yolov5_0.pt' --batch-size ${batch_size} --device 0 > ${cur_path}/test/output/$ASCEND_DEVICE_ID/train_eval_1p.log 2>&1 &
 
 wait
 
@@ -48,10 +44,10 @@ echo "end_time: ${end_time}"
 e2e_time=$(( $end_time - $start_time ))
 
 #最后一个迭代FPS值
-FPS=`grep -a 'FPS:'  ${cur_path}/test/output/$ASCEND_DEVICE_ID/train_perf_1p.log|awk 'END {print}'| awk -F "[" '{print $5}'| awk -F "]" '{print $1}'| awk -F ":" '{print $2}'`
+acc=`grep -a 'IoU=0.50:0.95' ${cur_path}/test/output/$ASCEND_DEVICE_ID/train_eval_1p.log|grep 'Average Precision'|awk 'END {print}'| awk -F " " '{print $13}'`
 
 #打印，不需要修改
-echo "ActualFPS : $FPS"
+echo "ActualAcc : $acc"
 echo "E2E Training Duration sec : $e2e_time"
 
 #稳定性精度看护结果汇总
@@ -59,10 +55,6 @@ echo "E2E Training Duration sec : $e2e_time"
 BatchSize=${batch_size}
 DeviceType=`uname -m`
 CaseName=${Network}_bs${BatchSize}_${RANK_SIZE}
-
-##获取性能数据，不需要修改
-#单迭代训练时长
-TrainingTime=`awk 'BEGIN{printf "%.2f\n", '${batch_size}'*1000/'${FPS}'}'`
 
 #关键信息打印到${CaseName}.log中，不需要修改
 echo "Network = ${Network}" > $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
