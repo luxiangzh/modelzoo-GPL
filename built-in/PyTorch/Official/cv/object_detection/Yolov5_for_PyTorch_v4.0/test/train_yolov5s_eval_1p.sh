894283e377f718cd111f80bb94d8953e75710f71
Ryan <wangzhiwei73@huawei.com>
2022-10-09 14:15:31 +0800

    统一修改acc获取命令

--- built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch_v4.0/test/train_yolov5s_eval_1p.sh
+++ built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch_v4.0/test/train_yolov5s_eval_1p.sh
@@ -44,7 +44,7 @@ echo "end_time: ${end_time}"
 e2e_time=$(( $end_time - $start_time ))
 
 #最后一个迭代FPS值
-acc=`grep -a 'IoU=0.50:0.95' ${cur_path}/test/output/$ASCEND_DEVICE_ID/train_eval_1p.log|grep 'Average Precision'|awk 'NR=1'| awk -F " " '{print $13}'`
+acc=`grep -a 'IoU=0.50:0.95' ${cur_path}/test/output/$ASCEND_DEVICE_ID/train_eval_1p.log|grep 'Average Precision'|awk 'NR==1'| awk -F " " '{print $13}'`
 
 #打印，不需要修改
 echo "ActualAcc : $acc"
--- built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch_v5.0/test/train_yolov5s_full_1p.sh
+++ built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch_v5.0/test/train_yolov5s_full_1p.sh
@@ -53,7 +53,7 @@ step_time=`grep -a 'step time:'  ${cur_path}/test/output/$ASCEND_DEVICE_ID/train
 FPS=`awk 'BEGIN{printf "%.2f\n", '${batch_size}'/'${step_time}'}'`
 
 #取acc值
-acc=`grep -a 'IoU=0.50:0.95' ${cur_path}/test/output/$ASCEND_DEVICE_ID/train_full_1p.log|grep 'Average Precision'|awk 'NR=1'| awk -F " " '{print $13}'`
+acc=`grep -a 'IoU=0.50:0.95' ${cur_path}/test/output/$ASCEND_DEVICE_ID/train_full_1p.log|grep 'Average Precision'|awk 'NR==1'| awk -F " " '{print $13}'`
 
 #打印，不需要修改
 echo "ActualFPS : $FPS"
