e8e0fc5a3afac66561e03707105e39f8160520b9
Evanism <1178580082@qq.com>
2022-10-09 06:30:21 +0000

    !70 【众智】【西安电子科技大学】【ID2949】【BERT4Rec-VAE】NPU精度和性能均达标，提交验收 !70 【众智】【西安电子科技大学】【ID2949】【BERT4Rec-VAE】NPU精度和性能均达标，提交验收

---
+++ contrib/PyTorch/Research/nlp/BERT4Rec-VAE_ID2949_for_PyTorch/datasets/ml_1m.py
@@ -0,0 +1,50 @@
+# Copyright 2021 Huawei Technologies Co., Ltd
+#
+# Licensed under the BSD 3-Clause License  (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://opensource.org/licenses/BSD-3-Clause
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from .base import AbstractDataset
+
+import pandas as pd
+
+from datetime import date
+
+
+class ML1MDataset(AbstractDataset):
+    @classmethod
+    def code(cls):
+        return 'ml-1m'
+
+    @classmethod
+    def url(cls):
+        return 'http://files.grouplens.org/datasets/movielens/ml-1m.zip'
+
+    @classmethod
+    def zip_file_content_is_folder(cls):
+        return True
+
+    @classmethod
+    def all_raw_file_names(cls):
+        return ['README',
+                'movies.dat',
+                'ratings.dat',
+                'users.dat']
+
+    def load_ratings_df(self):
+        folder_path = self._get_rawdata_folder_path()
+        file_path = folder_path.joinpath('ratings.dat')
+        df = pd.read_csv(file_path, sep='::', header=None)
+        df.columns = ['uid', 'sid', 'rating', 'timestamp']
+        return df
+
+
