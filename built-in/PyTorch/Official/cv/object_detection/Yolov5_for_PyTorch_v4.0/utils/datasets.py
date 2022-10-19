2f54105c8a746a47aff3bf240422dc082ce0e100
王智伟 <wangzhiwei73@huawei.com>
2022-10-19 03:39:29 +0000

    !88 [自研][Pytorch]yolov5统一整改cache文件命名，多tag兼容 * yolov5统一整改cache文件命名，多tag兼容

--- built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch_v4.0/utils/datasets.py
+++ built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch_v4.0/utils/datasets.py
@@ -399,7 +399,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
 
         # Check cache
         self.label_files = img2label_paths(self.img_files)  # labels
-        cache_path = Path(self.label_files[0]).parent.with_suffix('.cache')  # cached labels
+        cache_path = Path(str(Path(self.label_files[0]).parent) + '_yolov5_v4.0.cache')  # cached labels
         if cache_path.is_file():
             cache = torch.load(cache_path)  # load
             if cache['hash'] != get_hash(self.label_files + self.img_files) or 'results' not in cache:  # changed
