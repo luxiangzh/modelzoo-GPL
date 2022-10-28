d8226ef8eca3a8f49b02fa96415a13844079cb0e
魏鹏飞 <weipengfei19@huawei.com>
2022-10-28 11:56:14 +0000

    Complex_YOLOv4_Pytorch模型8p训练脚本适配ETP
    Signed-off-by: 魏鹏飞 <weipengfei19@huawei.com>

--- built-in/PyTorch/Official/cv/object_detection/Complex_YOLOv4_Pytorch/test/train_performance_8p.sh
+++ built-in/PyTorch/Official/cv/object_detection/Complex_YOLOv4_Pytorch/test/train_performance_8p.sh
@@ -1,5 +1,5 @@
 #网络名称,同目录名称,需要模型审视修改
-Network="Complex_yolov4"
+Network="Complex_ID4108_for_yolov4"
 # 数据集路径,保持为空,不需要修改
 data_path=""
 #当前路径,不需要修改
@@ -16,11 +16,37 @@ do
     fi
 done
 
+###############指定训练脚本执行路径###############
+# cd到与test文件夹同层级目录下执行脚本，提高兼容性；test_path_dir为包含test文件夹的路径
+cur_path_last_dirname=${cur_path##*/}
+if [ x"${cur_path_last_dirname}" == x"test" ]; then
+    test_path_dir=${cur_path}
+    cd ..
+    cur_path=$(pwd)
+else
+    test_path_dir=${cur_path}/test
+fi
+
+
+# 非平台场景时source 环境变量
+check_etp_flag=$(env | grep etp_running_flag)
+etp_flag=$(echo ${check_etp_flag#*=})
+if [ x"${etp_flag}" != x"true" ]; then
+    source ${test_path_dir}/env_npu.sh
+else
+    current_time=$(date +%s)
+    mkdir /npu/traindata/${current_time}
+    tar xzvf /${data_path}/kitti.tar.gz -C /npu/traindata/${current_time}/
+    data_path=/npu/traindata/${current_time}/kitti/
+fi
+
 if [ ! -d './dataset/kitti/training' ]
 then
 	ln -s ${data_path}/training/ ./dataset/kitti/training
 	ln -s ${data_path}/testing/ ./dataset/kitti/testing
 fi
+
+
 
 #训练开始时间，不需要修改
 start_time=$(date +%s)
@@ -32,10 +58,9 @@ if [ ! -d ${cur_path}/test/output/${device_id} ];then
     mkdir -p ${cur_path}/test/output/$device_id/
 fi
 
-source test/env_npu.sh
 cd src
 
-python3 -m torch.distributed.launch --nproc_per_node=${RANK_SIZE} train.py \
+python3.7 -m torch.distributed.launch --nproc_per_node=${RANK_SIZE} train.py \
      --dist-url 'tcp://127.0.0.1:29500' \
      --dist-backend 'hccl' \
      --multiprocessing-distributed \
@@ -77,6 +102,9 @@ echo "BatchSize = ${BatchSize}" >> $cur_path/test/output/$device_id/${CaseName}.
 echo "DeviceType = ${DeviceType}" >> $cur_path/test/output/$device_id/${CaseName}.log
 echo "CaseName = ${CaseName}" >> $cur_path/test/output/$device_id/${CaseName}.log
 echo "ActualFPS = ${FPS}" >> $cur_path/test/output/$device_id/${CaseName}.log
-echo "TrainingTime = ${TrainingTime}" >> $cur_path/test/output/$device_id/${CaseName}.log
+echo "TrainingTime = ${e2e_time}" >> $cur_path/test/output/$device_id/${CaseName}.log
 echo "ActualLoss = ${loss}" >> $cur_path/test/output/$device_id/${CaseName}.log
 echo "E2ETrainingTime = ${e2e_time}" >> $cur_path/test/output/$device_id/${CaseName}.log
+if [ x"${etp_flag}" = x"true" ]; then
+    rm -rf ${data_path}
+fi
