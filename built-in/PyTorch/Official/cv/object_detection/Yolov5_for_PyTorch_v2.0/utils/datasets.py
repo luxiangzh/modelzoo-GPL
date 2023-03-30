654cb29fc560262d803c592372218b73a2eeee0f
王智伟 <wangzhiwei73@huawei.com>
2023-03-30 12:47:58 +0000

    !177 [自研][Pytorch]Yolo系列更新worker计算逻辑为最新版本 * Yolo系列更新worker计算逻辑

--- built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch_v2.0/utils/datasets.py
+++ built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch_v2.0/utils/datasets.py
@@ -67,7 +67,8 @@ def create_dataloader(path, imgsz, batch_size, stride, opt, hyp=None, augment=Fa
                                     pad=pad)
 
     batch_size = min(batch_size, len(dataset))
-    nw = min([os.cpu_count() // world_size, batch_size if batch_size > 1 else 0, 12])  # number of workers
+    nd = torch.npu.device_count()
+    nw = min([os.cpu_count() // max(nd, 1), batch_size if batch_size > 1 else 0, workers])  # number of workers
     train_sampler = torch.utils.data.distributed.DistributedSampler(dataset) if local_rank != -1 else None
     dataloader = torch.utils.data.DataLoader(dataset,
                                     batch_size=batch_size,
