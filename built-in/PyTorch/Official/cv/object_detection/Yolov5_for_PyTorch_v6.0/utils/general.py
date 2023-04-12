bf73a656f64aa8ae4359e8bb7233e40f41769235
x-ting <chenruimin1@huawei.com>
2023-04-12 19:37:13 +0800

    复用YOLOV7适配的non_maximum_suppression函数，修复eval性能问题

--- built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch_v6.0/utils/general.py
+++ built-in/PyTorch/Official/cv/object_detection/Yolov5_for_PyTorch_v6.0/utils/general.py
@@ -622,6 +622,7 @@ def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=Non
 
     nc = prediction.shape[2] - 5  # number of classes
     xc = prediction[..., 4] > conf_thres  # candidates
+    device = prediction.device
 
     # Checks
     assert 0 <= conf_thres <= 1, f'Invalid Confidence threshold {conf_thres}, valid values are between 0.0 and 1.0'
@@ -657,6 +658,12 @@ def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=Non
         if not x.shape[0]:
             continue
 
+        n = x.shape[0]
+        n_max = int(np.ceil(n / 1024.)) * 1024
+        new_x = torch.zeros((n_max, x.shape[1]))
+        new_x[:n] = x
+        x = new_x.to(device)
+
         # Compute conf
         x[:, 5:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
 
@@ -665,11 +672,18 @@ def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=Non
 
         # Detections matrix nx6 (xyxy, conf, cls)
         if multi_label:
-            i, j = (x[:, 5:] > conf_thres).nonzero(as_tuple=False).T
-            x = torch.cat((box[i], x[i, j + 5, None], j[:, None].float()), 1)
+            x_cpu = x.cpu()
+            box_cpu = box.cpu()
+            i, j = (x_cpu[:, 5:] > conf_thres).nonzero(as_tuple=False).T
+            x = torch.cat((box_cpu[i], x_cpu[i, j + 5, None], j[:, None].float()), 1)
+            n = x.shape[0]
+            n_max = int(np.ceil(n / 1024.)) * 1024
+            new_x = torch.zeros((n_max, x.shape[1]))
+            new_x[:n] = x
+            x = new_x.to(device)
         else:  # best class only
             conf, j = x[:, 5:].max(1, keepdim=True)
-            x = torch.cat((box, conf, j.float()), 1)[conf.view(-1) > conf_thres]
+            x = torch.cat((box, conf, j.float()), 1)
 
         # Filter by class
         if classes is not None:
@@ -689,17 +703,26 @@ def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=Non
         # Batched NMS
         c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
         boxes, scores = x[:, :4] + c, x[:, 4]  # boxes (offset by class), scores
-        i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
+        if device.type == 'npu':
+            i, valid = torch.npu_nms_v4(boxes, scores, max_det, \
+                torch.tensor(iou_thres).to(device), torch.tensor(conf_thres).to(device))  # NMS
+            i = i.long().cpu()
+            valid = valid.cpu()
+            i = i[:valid]
+        else:
+            i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
         if i.shape[0] > max_det:  # limit detections
             i = i[:max_det]
         if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
             # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
+            boxes = boxes.cpu()
+            scores = scores.cpu()
             iou = box_iou(boxes[i], boxes) > iou_thres  # iou matrix
             weights = iou * scores[None]  # box weights
             x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(1, keepdim=True)  # merged boxes
             if redundant:
                 i = i[iou.sum(1) > 1]  # require redundancy
-
+        x = x.cpu()
         output[xi] = x[i]
         if (time.time() - t) > time_limit:
             print(f'WARNING: NMS time limit {time_limit}s exceeded')
