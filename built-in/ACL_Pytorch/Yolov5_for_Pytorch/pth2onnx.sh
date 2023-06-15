0ed967296492ed2507d3e0981a42e9590f619337
WangZikai <wangzikai@huawei.com>
2023-06-15 14:13:02 +0800

    chore:取消对onnxsim的安装与使用
    删除环境配置文件requirements.txt中onnxsim相关依赖的安装
    删除转onnx脚本pth2onnx.sh脚本中对onnxsim的使用

--- built-in/ACL_Pytorch/Yolov5_for_Pytorch/pth2onnx.sh
+++ built-in/ACL_Pytorch/Yolov5_for_Pytorch/pth2onnx.sh
@@ -28,7 +28,6 @@ if [[ $nms_mode == nms_script ]] ; then
     git apply common/patch/v${tag}.patch --exclude models/yolo.py ## 兼容性修改，支持导出不同版本的YOLOv5
     export=`find ./ -name export.py`  ## 查找export.py文件路径，不同版本路径不一样
     python3 ${export} --weights=${model}.pt --opset=11 --dynamic || exit 1  # 导出onnx模型
-    python3 -m onnxsim ${model}.onnx ${model}.onnx || exit 1  # 简化导出的onnx模型
 fi
 
 ##### 方式二 nms后处理算子
@@ -37,7 +36,6 @@ if [[ $nms_mode == nms_op ]] ; then
     git apply common/patch/v${tag}.patch  ## 兼容性修改，支持导出不同版本的YOLOv5，Detect部分修改以适配添加nms后处理算子
     export=`find ./ -name export.py`  ## 查找export.py文件路径，不同版本路径不一样
     python3 ${export} --weights=${model}.pt --opset=11 --dynamic || exit 1  # 导出onnx模型
-    python3 -m onnxsim ${model}.onnx ${model}.onnx || exit 1  # 简化导出的onnx模型
     python3 common/util/add_nms.py --pt=${model}.pt --onnx=${model}.onnx --cfg=model.yaml || exit 1  ## 修改模型，添加nms后处理算子
 fi
 
